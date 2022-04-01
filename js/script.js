a1b70e55bc45facbf32000ec498d89f3e03aabeb
Angela <angelagraham101@gamil.com>
2022-04-01 16:44:15 -0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let answersLength = 20
+  let zero ="images/magic8ball_1.png"
+  let one ="images/magic8ball_2.png"
+  let two ="images/magic8ball_3.png"
+  let three = "images/magic8ball_4.png"
+  let four ="images/magic8ball_5.png"
+  let five = "images/magic8ball_6.png"
+  let six ="images/magic8ball_7.png"
+  let seven = "images/magic8ball_8.png"
+  let eight ="images/magic8ball_9.png"
+  let nine = "images/magic8ball_10.png"
+  let ten ="images/magic8ball_11.png"
+  let eleven ="images/magic8ball_12.png"
+  let twelve ="images/magic8ball_13.png"
+  let thirteen ="images/magic8ball_14.png"
+  let fourteen ="images/magic8ball_15.png"
+  let fifteen ="images/magic8ball_16.png"
+  let sixteen ="images/magic8ball_17.png"
+  let seventeen ="images/magic8ball_18.png"
+  let eighteen ="images/magic8ball_19.png"
+  let nineteen = "images/magic8ball_20.png"
+
+let answersImages = [zero, one, two, three, four, 
+  five, six, seven, eight, nine, ten, eleven, tweleve,
+  thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen];
+
+  function question(text) 
+  function ask() {
+      return Math.floor(Math.random()*answersRandomImagesLength);
+  }
+  let ask = document.getElementById("button");
+  ask.addEventListener("click", ask);
+
+    // (let randomAnswers = document.getElementById("8ball").src = `./magic/magic8ball_${randomNumber}`.png)
+    //  let randomNumber = Math.floor(Math.random()* answers.length ++);
+    //  for (i=0; i=<20;i++) 
+    //  answers[i]
+ 
+    // function ask(text) 
+    // function random() {
+    // return Math.floor(Math.random()*number);
+    // }
+    //           
+    
+    // random_index = Math.floor(Math.random()*images.length);
+    //  selected_images =images[random_index]
+    //  document.getElementById("").scr = ./${selected_images}
+    
+
+// let answers = ["It is certain", "It is decidely so.", "Yes definitely.",
+// "Without a doubt.", "Yes you may rely on it.", "As I see it, yes.", 
+//  "Most Likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Try again.", 
+//   "Ask again later.", "Better not tell you now.", "Cannot predict now.", 
+//   "Concentrate and ask again.", "Don't count on it.", "My reply is no.", 
+//   "My sources say no.", "Outlook not so good.", "Very doubtful.", ]
+
+//   return images
+
+
+// let answersLength = 20
+// function ask(){
+//     let randomNumber = Math.floor(Math.random()* answersLength +1);
+//     let randomImage = document.getElementById("myImg").scr = ./images/magic8ball_${randomNumber}.png
+
+// }
+
+// const answers = document.querySelctor
+//     function answers()
+//     let a = Math.floor(Math.random()answers.length);
+//     let img = magic8ball [a];
